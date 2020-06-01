1f138148114f9c2dc216f3e8197e5e5ae9c49714
June Jung <costalgia@gmail.com>
2020-06-01 12:16:21 -0700

    adding socket.io

---
+++ chat-app/public/js/chat.js
@@ -0,0 +1,26 @@
+const socket = io()
+
+socket.on('message', (message) => {
+  console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  const message = e.target.elements.message.value
+  socket.emit('sendMessage', message)
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by yor browser')
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+    
+    socket.emit('sendLocation', { latitude, longitude })
+
+  })
+})
+
